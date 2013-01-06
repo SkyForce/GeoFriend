8d843bbb2abf91e979b5b05cf7bfeb94812ab341
ForNeVeR <neverthness@gmail.com>
2013-01-06 18:48:19 +0700

    Server: spaces to tabs.

--- Server/src/ru/allgage/geofriend/server/SocketHandler.java
+++ Server/src/ru/allgage/geofriend/server/SocketHandler.java
@@ -15,69 +15,69 @@ import java.util.Random;
  * To change this template use File | Settings | File Templates.
  */
 public class SocketHandler implements Runnable {
-    Socket sock;
-    boolean isLogged = false;
-    UserDAO userDAO;
+	Socket sock;
+	boolean isLogged = false;
+	UserDAO userDAO;
 
-    SocketHandler(Socket sock, UserDAO userDAO) {
-        this.sock = sock;
-        this.userDAO = userDAO;
-        System.out.println(sock.getInetAddress().toString());
-    }
+	SocketHandler(Socket sock, UserDAO userDAO) {
+		this.sock = sock;
+		this.userDAO = userDAO;
+		System.out.println(sock.getInetAddress().toString());
+	}
 
-    public void run() {
-        try (Socket socket = sock;
-             DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
-             DataInputStream din = new DataInputStream(socket.getInputStream())) {
+	public void run() {
+		try (Socket socket = sock;
+			 DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
+			 DataInputStream din = new DataInputStream(socket.getInputStream())) {
 
-            String auth = din.readUTF();
+			String auth = din.readUTF();
 
-            if(auth.equals("login")) {
-                String user = din.readUTF();
-                String pass = din.readUTF();
+			if(auth.equals("login")) {
+				String user = din.readUTF();
+				String pass = din.readUTF();
 
-                isLogged = userDAO.exist(user, pass);
-            } else if(auth.equals("register")) {
-                String user = din.readUTF();
-                String pass = din.readUTF();
-                String email = din.readUTF();
+				isLogged = userDAO.exist(user, pass);
+			} else if(auth.equals("register")) {
+				String user = din.readUTF();
+				String pass = din.readUTF();
+				String email = din.readUTF();
 
-                isLogged = userDAO.create(user, pass, email);
-            }
+				isLogged = userDAO.create(user, pass, email);
+			}
 
-            if(isLogged) {
-                dout.writeUTF("logged in");
-            }
-            else {
-                dout.writeUTF("error");
-                return;
-            }
-            dout.flush();
-            String str = din.readUTF();
-            Random rnd = new Random();
+			if(isLogged) {
+				dout.writeUTF("logged in");
+			}
+			else {
+				dout.writeUTF("error");
+				return;
+			}
+			dout.flush();
+			String str = din.readUTF();
+			Random rnd = new Random();
 
-            while(true) {
-                dout.writeUTF(String.format("Twice:%s:%s:lol", String.valueOf(rnd.nextInt(180)-90), String.valueOf(rnd.nextInt(360)-180)));
-                dout.flush();
-                din.readUTF();
-                Thread.sleep(2000);
-            }
-            /**String msg="";
+			while(true) {
+				dout.writeUTF(String.format("Twice:%s:%s:lol", String.valueOf(rnd.nextInt(180)-90), String.valueOf(rnd.nextInt(360)-180)));
+				dout.flush();
+				din.readUTF();
+				Thread.sleep(2000);
+			}
+			/**String msg="";
 
-            while(!msg.equals("exit")) {
-                msg = din.readUTF();
-                System.out.println(msg);
-                dout.writeUTF("Got your msg "+msg);
-                dout.flush();
-            }**/
+			while(!msg.equals("exit")) {
+				msg = din.readUTF();
+				System.out.println(msg);
+				dout.writeUTF("Got your msg "+msg);
+				dout.flush();
+			}**/
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+		}
 
-    }
+	}
 }
