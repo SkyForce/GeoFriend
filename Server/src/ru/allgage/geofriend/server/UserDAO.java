8c6a160c5bb826c9a3ce85f2d2e1726b6227c0ba
ForNeVeR <neverthness@gmail.com>
2013-01-06 18:44:58 +0700

    Server: spaces to tabs.

--- Server/src/ru/allgage/geofriend/server/UserDAO.java
+++ Server/src/ru/allgage/geofriend/server/UserDAO.java
@@ -8,51 +8,51 @@ import java.sql.SQLException;
  * User data access object.
  */
 public class UserDAO {
-    private Connection connection;
+	private Connection connection;
 
-    /**
-     * Create data access object instance.
-     * @param connection server connection.
-     */
-    public UserDAO(Connection connection) {
-        this.connection = connection;
-    }
+	/**
+	 * Create data access object instance.
+	 * @param connection server connection.
+	 */
+	public UserDAO(Connection connection) {
+		this.connection = connection;
+	}
 
-    /**
-     * Check whether a user with specified login and password exist in the database.
-     * @param login user name.
-     * @param password user password.
-     * @return true if user exist.
-     * @throws SQLException thrown on query fail.
-     */
-    public boolean exist(String login, String password) throws SQLException {
-        try (PreparedStatement statement = connection.prepareStatement(
-                "SELECT * FROM users WHERE login = ? AND password = MD5(?)")) {
-            statement.setString(1, login);
-            statement.setString(2, password);
+	/**
+	 * Check whether a user with specified login and password exist in the database.
+	 * @param login user name.
+	 * @param password user password.
+	 * @return true if user exist.
+	 * @throws SQLException thrown on query fail.
+	 */
+	public boolean exist(String login, String password) throws SQLException {
+		try (PreparedStatement statement = connection.prepareStatement(
+				"SELECT * FROM users WHERE login = ? AND password = MD5(?)")) {
+			statement.setString(1, login);
+			statement.setString(2, password);
 
-            return statement.execute();
-        }
-    }
+			return statement.execute();
+		}
+	}
 
-    /**
-     * Creates the user.
-     * @param login user name.
-     * @param password user password.
-     * @param email user e-mail.
-     * @return true if the user was created; false otherwise.
-     */
-    public boolean create(String login, String password, String email) {
-        try (PreparedStatement statement = connection.prepareStatement(
-                "INSERT INTO users (login, password, email) VALUES(?, MD5(?), ?)")) {
-            statement.setString(1, login);
-            statement.setString(2, password);
-            statement.setString(3, email);
+	/**
+	 * Creates the user.
+	 * @param login user name.
+	 * @param password user password.
+	 * @param email user e-mail.
+	 * @return true if the user was created; false otherwise.
+	 */
+	public boolean create(String login, String password, String email) {
+		try (PreparedStatement statement = connection.prepareStatement(
+				"INSERT INTO users (login, password, email) VALUES(?, MD5(?), ?)")) {
+			statement.setString(1, login);
+			statement.setString(2, password);
+			statement.setString(3, email);
 
-            return statement.executeUpdate() > 0;
-        } catch (SQLException exception) {
-            exception.printStackTrace();
-            return false;
-        }
-    }
+			return statement.executeUpdate() > 0;
+		} catch (SQLException exception) {
+			exception.printStackTrace();
+			return false;
+		}
+	}
 }
