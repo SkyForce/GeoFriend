23a92b1e63e190289d0ab6cde6aeaadb7c81804e
ForNeVeR <neverthness@gmail.com>
2013-01-07 15:01:39 +0700

    Server: added Status class and DAO.

---
+++ Server/src/ru/allgage/geofriend/server/SQLHelper.java
@@ -0,0 +1,11 @@
+package ru.allgage.geofriend.server;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ForNeVeR
+ * Date: 07.01.13
+ * Time: 14:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class SQLHelper {
+}
